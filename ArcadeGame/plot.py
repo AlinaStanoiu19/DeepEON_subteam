7ab4f9cdb1a333a919dda1de143c5c5dc3037e03
Rafael Miguel Asensio Delgado <zceeras@turin.ee.ucl.ac.uk>
2023-03-14 10:44:56 +0000

    push pre plot

--- ArcadeGame/plot.py
+++ ArcadeGame/plot.py
@@ -5,22 +5,22 @@ import numpy as np
 
 df1 = pd.read_json("Evaluations/heuristic_evaluation_8_24_5_10_-10_-15_0_3_2_16_8_1000.json")      # heuristics
 df2 = pd.read_json("Evaluations/agent_evaluation_8_24_5_10_-10_-15_0_3_2_16_8_1000.json")   # DQL arrows
-# df3 = pd.read_json("Evaluation_data/evaluation_neat-gorge-5.json")      # DQL slot selection
+df3 = pd.read_json("Evaluation_data/agent_evaluation_8_24_5_10_-10_-15_0_4_2_16_8_1000.json")      # DQL slot selection
 
 print("Heuristics performance")
 mean_reward1 = np.mean(df1["Episode Rewards"])
 std_reward1 = np.std(df1["Episode Rewards"])
 print(f"Mean Reward: {mean_reward1}, STD Reward: {std_reward1}")
 
-print("DQL arrows performance")
+print("DQL normal performance")
 mean_reward2 = np.mean(df2["Episode Rewards"])
 std_reward2 = np.std(df2["Episode Rewards"])
 print(f"Mean Reward: {mean_reward2}, STD Reward: {std_reward2}")
 
-# print("DQL slot selection performance")
-# mean_reward3 = np.mean(df3["Episode Rewards"])
-# std_reward3 = np.std(df3["Episode Rewards"])
-# print(f"Mean Reward: {mean_reward3}, STD Reward: {std_reward3}")
+print("DQL path visualisation performance")
+mean_reward3 = np.mean(df3["Episode Rewards"])
+std_reward3 = np.std(df3["Episode Rewards"])
+print(f"Mean Reward: {mean_reward3}, STD Reward: {std_reward3}")
 
 # Start plotting
 
@@ -72,21 +72,21 @@ for i, er in enumerate(df2["Episode Rewards"]):
         avg = a / num_avg
         avg_df2.append(avg)
         a = 0
-# a = 0
-# for i, er in enumerate(df3["Episode Rewards"]):
-#     a += er
-#     if (i+1) % num_avg == 0:
-#         avg = a / num_avg
-#         avg_df3.append(avg)
-#         a = 0
+a = 0
+for i, er in enumerate(df3["Episode Rewards"]):
+    a += er
+    if (i+1) % num_avg == 0:
+        avg = a / num_avg
+        avg_df3.append(avg)
+        a = 0
 
 ax.plot(new_eps, avg_df1, label="Heuristics", linewidth=0.5, color="red")
 ax.plot(new_eps, avg_df2, label="DeepEON Game 3", linewidth=0.5, color="navy")
-#ax.plot(new_eps, avg_df3, label="DeepEON SL", linewidth=0.5, color="deepskyblue")
+ax.plot(new_eps, avg_df3, label="DeepEON Gane 4", linewidth=0.5, color="deepskyblue")
 
 ax.plot(new_eps, np.full((y_avg,), mean_reward1), linewidth=2, color="red")
 ax.plot(new_eps, np.full((y_avg,), mean_reward2), linewidth=2, color="navy")
-#ax.plot(new_eps, np.full((y_avg,), mean_reward3), linewidth=2, color="deepskyblue")
+ax.plot(new_eps, np.full((y_avg,), mean_reward3), linewidth=2, color="deepskyblue")
 
 plt.legend()
 
